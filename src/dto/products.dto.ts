3974c27ab47fa955c6d1710947ea6e4aec2cde73
Christian <c.espinoza.code@gmail.com>
2023-01-07 12:28:10 -0500

    createProductDto validated with class-validator

--- src/dto/products.dto.ts
+++ src/dto/products.dto.ts
@@ -1,12 +1,31 @@
-export interface CreateProductDto {
+import {
+  IsString,
+  IsNumber,
+  IsPositive,
+  IsUrl,
+  IsNotEmpty,
+} from 'class-validator';
+
+export class CreateProductDto {
+  @IsString()
   readonly name: string;
+
+  @IsString()
   readonly description: string;
+
+  @IsNumber()
+  @IsPositive()
   readonly price: number;
+
+  @IsNumber()
+  @IsPositive()
   readonly stock: number;
+
+  @IsUrl()
   readonly image: string;
 }
 
-export interface UpdateProductDto {
+export class UpdateProductDto {
   readonly name?: string;
   readonly description?: string;
   readonly price?: number;
